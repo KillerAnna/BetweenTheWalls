2212180e7627aa44f25853caebb42a040f66ec9d
ColorPS <rlatpwjd1369@naver.com>
2021-06-06 17:14:10 +0900

    6/6
    - 교한이가 한거 추가
    - Enemy 오브젝트 다시 삭제

--- Assets/Script/Item/Item_Spawn.cs
+++ Assets/Script/Item/Item_Spawn.cs
@@ -20,7 +20,7 @@ public class Item_Spawn : MonoBehaviour // 총알 아이템 생성
             {
                 if (col.gameObject.layer == LayerMask.NameToLayer("Wall")) // 총알 아이템 생성 위치가 벽이라면
                     continue_spawn = true; // 다시 생성
-                else if (col.gameObject.layer == LayerMask.NameToLayer("Floor_Monster")) // 총알 아이템 생성 위치가 몬스터스폰 이라면
+                else if (col.gameObject.layer == LayerMask.NameToLayer("Floor_Spawn")) // 총알 아이템 생성 위치가 몬스터스폰 이라면
                     continue_spawn = true; // 다시 생성
                 else if (col.tag.Equals("Item_Bullet")) // 총알 아이템 생성 위치에 총알 아이템이 이미 있다면
                     continue_spawn = true; // 다시 생성
